79082e628b7df4ab35e57e258c21caf48f0f3ed8
Isaac Good <github@isaacgood.com>
2023-04-16 13:49:32 -0700

    MentorRequests: factor out the data load and add a command to trigger a sync

--- cogs/mentor_requests.py
+++ cogs/mentor_requests.py
@@ -75,45 +75,60 @@ class RequestNotifier(commands.Cog):
         for request_id, (track, message) in list(self.requests.items()):
             if request_id in current_request_ids:
                 continue
-            del self.requests[request_id]
             await message.delete()
+            del self.requests[request_id]
             self.conn.execute(QUERY["del_request"], {"request_id": request_id})
 
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         """Fetch tracks and configure threads as needed."""
+        await self.load_data()
+        self.update_mentor_requests.start()  # pylint: disable=E1101
+
+    @commands.is_owner()
+    @commands.dm_only()
+    @commands.command()
+    async def requests_reload(self, ctx: commands.Context) -> None:
+        """Command to reload data."""
+        _ = ctx  # unused
+        await self.load_data()
+
+    async def load_data(self) -> None:
+        """Load Exercism data."""
         guild = self.bot.get_guild(self.exercism_guild_id)
-        if not self.tracks:
-            self.tracks = await self.exercism.all_tracks()
+        assert guild is not None
+        self.tracks = await self.exercism.all_tracks()
         self.tracks.sort()
+
         cur = self.conn.execute(QUERY["get_theads"])
-        self.threads = {
-            track_slug: guild.get_thread(message_id)
-            for track_slug, message_id in cur.fetchall()
-        }
+        self.threads = {}
+        for track_slug, message_id in cur.fetchall():
+            thread = guild.get_thread(message_id)
+            assert thread is not None
+            self.threads[track_slug] = thread
 
         channel = guild.get_channel(self.channel_id)
+        assert isinstance(channel, discord.TextChannel)
         for track in self.tracks:
             if track in self.threads:
                 continue
-            message = await channel.create_thread(
+            thread = await channel.create_thread(
                 name=track.title(),
                 type=discord.ChannelType.public_thread,
             )
             self.conn.execute(
                 QUERY["add_thead"],
-                {"track_slug": track, "message_id": message.id},
+                {"track_slug": track, "message_id": thread.id},
             )
-            self.threads[track] = message
+            self.threads[track] = thread
             await asyncio.sleep(5)
 
         cur = self.conn.execute(QUERY["get_requests"])
-        self.requests = {
-            request_id: (track_slug, await self.threads[track_slug].fetch_message(message_id))
-            for request_id, track_slug, message_id in cur.fetchall()
-        }
-
-        self.update_mentor_requests.start()  # pylint: disable=E1101
+        self.requests = {}
+        for request_id, track_slug, message_id in cur.fetchall():
+            message = await self.threads[track_slug].fetch_message(message_id)
+            assert message is not None
+            self.requests[request_id] = message
 
     async def get_requests(self, track_slug: str) -> dict[str, str]:
         """Return formatted mentor requests."""
