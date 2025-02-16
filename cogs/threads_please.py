290af86feb1150d356efef2797abe5b78a45abba
Isaac Good <github@isaacgood.com>
2025-02-16 15:09:05 -0800

    Add thread reminders

---
+++ cogs/threads_please.py
@@ -0,0 +1,55 @@
+"""Discord Cog to remind people to use threads."""
+
+import logging
+from typing import cast
+
+import discord
+import prometheus_client  # type: ignore
+from discord.ext import commands
+
+from cogs import base_cog
+
+logger = logging.getLogger(__name__)
+
+TITLE = "Reminder: Please Use Threads"
+REMINDER = (
+    "Please use threads in this channel. "
+    "Rather than replying to a message, please create a thread."
+)
+DURATION = 120
+
+
+class ThreadReminder(base_cog.BaseCog):
+    """Reminds people using "reply to" to use threads."""
+
+    qualified_name = TITLE
+
+    def __init__(
+        self,
+        *,
+        channels: list[int],
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.channels = channels
+        self.prom_counter = prometheus_client.Counter(
+            "thread_reminder", "How many times thread reminder was triggered."
+        )
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        """React to non-threaded responses with a reminder."""
+        channel = message.channel
+        if message.author.bot or message.reference is None:
+            return
+        if channel is None or channel.type != discord.ChannelType.text:
+            return
+        if channel.id not in self.channels:
+            return
+
+        self.usage_stats[message.author.display_name] += 1
+        self.prom_counter.inc()
+        typed_channel = cast(discord.TextChannel, channel)
+        thread = await typed_channel.create_thread(name=TITLE, auto_archive_duration=60)
+        content = f"{message.author.mention} {REMINDER}\n\n{message.jump_url}"
+        await thread.send(content=content, suppress_embeds=True)
